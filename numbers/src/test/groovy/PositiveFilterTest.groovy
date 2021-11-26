5087a62561208747076fc099974ef7fdef82ccd1
ianitalo <ianitalo56@hotmail.com>
2021-11-26 15:57:47 +0000

    PositiveFilterTest.groovy done

--- numbers/src/test/groovy/PositiveFilterTest.groovy
+++ numbers/src/test/groovy/PositiveFilterTest.groovy
@@ -1,2 +1,16 @@
-class PositiveFilterTest {
+import com.aor.numbers.PositiveFilter
+import spock.lang.Specification
+
+class PositiveFilterTest extends Specification
+{
+    PositiveFilter filtro = new PositiveFilter()
+    def 'Accept'()
+    {
+        given:
+        Integer testnumber = 2
+        when:
+        def res = filtro.accept(testnumber)
+        then:
+        res == true
+    }
 }
