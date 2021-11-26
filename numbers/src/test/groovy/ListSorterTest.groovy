d0880d82c4ca1e23e5991d0e185f5eefa29c3e96
ianitalo <ianitalo56@hotmail.com>
2021-11-26 15:51:36 +0000

    ListSorterTest.groovy done

--- numbers/src/test/groovy/ListSorterTest.groovy
+++ numbers/src/test/groovy/ListSorterTest.groovy
@@ -1,2 +1,21 @@
-class ListSorterTest {
+import com.aor.numbers.ListSorter
+import spock.lang.Specification
+
+class ListSorterTest extends Specification
+{
+    private List<Integer> list,expected
+    def setup()
+    {
+        list = Arrays.asList(3, 2, 6, 1,25, 4, 5, 7,8,9,1,2,3,8,4)
+        expected = Arrays.asList(1,1,2, 2,3, 3, 4,4, 5, 6, 7,8,8,9,25)
+    }
+    def 'sort'()
+    {
+        given:
+        ListSorter sorter = new ListSorter()
+        when:
+        List<Integer> sorted = sorter.sort(list)
+        then:
+        sorted == expected
+    }
 }
