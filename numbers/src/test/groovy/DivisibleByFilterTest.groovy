cc78374539ef06375a3190a61578a7bd7dfce9f2
ianitalo <ianitalo56@hotmail.com>
2021-11-26 16:14:05 +0000

    DivisibleByFilterTest.groovy done

--- numbers/src/test/groovy/DivisibleByFilterTest.groovy
+++ numbers/src/test/groovy/DivisibleByFilterTest.groovy
@@ -1,2 +1,19 @@
-class DivisibleByFilterTest {
+import com.aor.numbers.DivisibleByFilter
+import spock.lang.Specification
+
+class DivisibleByFilterTest extends Specification
+{
+    private DivisibleByFilter filtro
+    def 'Accept'()
+    {
+        given:
+        Integer testnumber = 9
+        Integer divisor = 2
+        when:
+        filtro = new DivisibleByFilter(divisor)
+        def res = filtro.accept(testnumber)
+        then:
+        res == false
+
+    }
 }
